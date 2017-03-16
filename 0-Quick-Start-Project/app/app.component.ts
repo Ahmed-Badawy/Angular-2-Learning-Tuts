61eed51bffa6e195f2faa96c3d3d15a2bd63f84f
Ahmed Badawy <me@ahmed-badawy.com>
2017-03-16 16:20:52 +0300

    all

--- 0-Quick-Start-Project/app/app.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'my-app',
-  template: `<h1>My First Angular App</h1>
-  	<h3>really this is not my first app</h3>
-  `
-})
-export class AppComponent { }
